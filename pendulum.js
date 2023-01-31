83535c82432cec3221c9b858e087ea7d0cd65377
Giorgio Reveco <40617591+girebz@users.noreply.github.com>
2023-01-31 10:58:22 -0300

    He subido los archivos pendulum.html y pendulum.js

---
+++ pendulum.js
@@ -0,0 +1,33 @@
+const canvas = document.getElementById("canvas");
+const context = canvas.getContext("2d");
+
+let t = 0;
+
+const a = 100;
+const b = 100;
+const c = 75;
+const d = 75;
+const phaseDifference = Math.PI / 4;
+
+function drawPendulum() {
+  context.clearRect(0, 0, canvas.width, canvas.height);
+
+  const x1 = a * Math.sin(t);
+  const y1 = b * Math.cos(t);
+
+  const x2 = c * Math.sin(t + phaseDifference);
+  const y2 = d * Math.cos(t + phaseDifference);
+
+  context.beginPath();
+  context.arc(x1 + canvas.width / 2, canvas.height / 2 - y1, 10, 0, 2 * Math.PI);
+  context.fill();
+
+  context.beginPath();
+  context.arc(x2 + canvas.width / 2, canvas.height / 2 - y2, 10, 0, 2 * Math.PI);
+  context.fill();
+
+  t += 0.05;
+  requestAnimationFrame(drawPendulum);
+}
+
+drawPendulum();
