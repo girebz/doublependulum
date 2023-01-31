e6f9175907376aef5dd2ff4159ed4fb04f5fcfb7
Giorgio Reveco <40617591+girebz@users.noreply.github.com>
2023-01-31 13:20:52 -0300

    Update pendulum.js

--- pendulum.js
+++ pendulum.js
@@ -19,16 +19,16 @@ function drawPendulum() {
   const y2 = d * Math.cos(t + phaseDifference);
 
   context.beginPath();
-  context.arc(x1 + canvas.width / 2, canvas.height / 2 - y1, 10, 0, 2 * Math.PI);
+  context.arc(x1 + canvas.width / 10, canvas.height / 2 - y1, 10, 0, 2 * Math.PI);
   context.fill();
 
   context.beginPath();
-  context.arc(x2 + canvas.width / 2, canvas.height / 2 - y2, 10, 0, 2 * Math.PI);
+  context.arc(x2 + canvas.width / 10, canvas.height / 2 - y2, 10, 0, 2 * Math.PI);
   context.fill();
 
   context.beginPath();
-  context.moveTo(x1 + canvas.width / 2, canvas.height / 2 - y1);
-  context.lineTo(x2 + canvas.width / 2, canvas.height / 2 - y2);
+  context.moveTo(x1 + canvas.width / 10, canvas.height / 5 - y1);
+  context.lineTo(x2 + canvas.width / 10, canvas.height / 5 - y2);
   context.stroke();
 
   t += 0.05;
